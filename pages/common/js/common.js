1e4a8420f1ef67733fab4c88480968e2b937b3d6
cizt <cizt@strongdata.com.cn>
2020-04-21 11:33:13 +0800

    柱形图简单版封装

--- pages/common/js/common.js
+++ pages/common/js/common.js
@@ -2,7 +2,8 @@
  * 获取url参数
  * @param name: 参数名称
  */
-$.getUrlParam = function(name){
+var $c = {};//自定义插件库
+$c.getUrlParam = function(name){
 	try{
 		var webUrl = window.location.href;
 		if(webUrl.indexOf("?") == -1){
@@ -24,14 +25,14 @@ $.getUrlParam = function(name){
  * 字符串编码
  * @param e: 字符串
  */
-$.encode = function(e){
+$c.encode = function(e){
 	return encodeURIComponent(e || "");
 }
 /**
  * 字符串解码
  * @param e: 字符串
  */
-$.decode = function(e){
+$c.decode = function(e){
 	return decodeURIComponent(e || "");
 }
 /**
@@ -90,3 +91,62 @@ var confirm = function(text, func, cancelFunc) {
 	$(".confirm-dialog-con").css("margin-top" , marginT + "px");
 }
 
+/**	charts库 begin */
+$c.charts = {};
+/**
+ * 柱形图
+ * @params id: 绑定id , 必须
+ * @params datas: 数据列表,例: [{name: '柱名称', data: [1, 2, 3, ...]}, ...] , 必须
+ * @params xLabels: x轴文字显示列表,例: ['文字1', '文字2', '文字3', ...] , 必须
+ * @params colors: 柱颜色 , 例: ['#428EDA', '#87D568', ...] , 可选
+ * */
+$c.charts.zhu = function(id, datas, xLabels, colors) {
+	datas = datas || [];
+	xLabels = xLabels || [];
+	colors = colors || ['#428EDA', '#87D568', '#FF696B', '#7F77E6', '#D8A7DE', '#FCCD57'];
+	Highcharts.chart(id,{
+		chart: {
+			type: 'column',
+			backgroundColor: 'none'
+		},
+		title: {
+			text: ''
+		},
+		subtitle: {
+			text: ''
+		},
+		xAxis: {
+			categories: xLabels,
+			labels: {
+				style: {
+					color: '#fff'
+				}
+			},
+			crosshair: true
+		},
+		yAxis: {
+			min: 0,
+			title: {
+				text: ''
+			},
+			labels: {
+				style: {
+					color: '#fff'
+				}
+			}
+		},
+		plotOptions: {
+			column: {
+				borderWidth: 0,
+			}
+		},
+		legend: {
+			enabled: false
+		},
+		series: datas,
+		colors: colors
+	});
+	$('#'+ id +' .highcharts-credits').remove();
+}
+
+/**	charts库 end */
--- pages/common/js/head.js
+++ pages/common/js/head.js
@@ -6,6 +6,7 @@ document.writeln('<meta http-equiv="expires" content="0">');
 document.writeln('<meta http-equiv="X-UA-Compatible" content="IE=edge">');
 document.writeln('<link type="text/css" rel="stylesheet" media="all" href="'+ baseUrl +'/common/css/common.css?v='+ v +'" />');
 document.writeln('<script type="text/javascript" src="'+ baseUrl +'/common/js/jquery-1.11.3.min.js"></script>');
+document.writeln('<script type="text/javascript" src="'+ baseUrl +'/common/highchats/highcharts.js"></script>');
 document.writeln('<script type="text/javascript" src="'+ baseUrl +'/common/js/store.js"></script>');
 document.writeln('<script type="text/javascript" src="'+ baseUrl +'/common/js/common.js?v='+ v +'"></script>');
 document.writeln('<script type="text/javascript" src="'+ baseUrl +'/common/json/json.js?v='+ v +'"></script>');
